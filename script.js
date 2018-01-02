9c39feeae2ab92fc54f7054f1adc918d4522517d
Nicholas Paxford <NickPax@users.noreply.github.com>
2018-01-02 15:29:38 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,89 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul")
+var deleteBtns = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
+
+
+
+//add event listener to first 6 btns in HTML file
+for(var i = 0; i < deleteBtns.length; i++){
+	deleteBtns[i].addEventListener("click", removeParent);
+}
+
+
+//from StackOverflow:
+function removeParent(evt) {
+  evt.target.parentNode.remove();
+}
+
+//click on a list item and it strikethroughs the text
+function getEventTarget(e){
+	e = e || window.event;
+	return e.target || e.srcElement;
+}
+
+
+ul.onclick = function(event){
+	var target = getEventTarget(event);
+	target.classList.toggle("done");
+	
+}
+
+
+//adding new items:
+
+function inputLength(){
+	return input.value.length;
+}
+
+function createListElement() {
+	var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
+
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	li.innerHTML = li.innerHTML + " ";
+	li.appendChild(btn);
+
+	ul.appendChild(li);
+	input.value="";
+}
+
+
+function addToListAfterClick(){
+	if(inputLength() > 0){
+			createListElement();
+		}
+}
+
+function addToListAfterKeypress(event){
+	if(inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+
+button.addEventListener("click", addToListAfterClick);
+
+input.addEventListener("keypress", addToListAfterKeypress);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
